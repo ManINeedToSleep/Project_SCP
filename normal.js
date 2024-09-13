17c88c05af6eb57e84d1c71a2d2974707a47e590
ManIWantToSleep <bguna0050@launchpadphilly.org>
2024-09-13 14:41:28 -0400

    Everything part 3

--- normal.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var loadingScreen = document.getElementById('loading-screen');
-    var mainContent = document.getElementById('main-content');
-    
-    setTimeout(function() {
-        loadingScreen.style.display = 'none'; // Hide the loading screen
-        mainContent.style.display = 'block';  // Show the main content
-    }, 0); // 10 seconds delay
-});
---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    var mainContent = document.getElementById('main-content');
+    
+    setTimeout(function() {
+        loadingScreen.style.display = 'none'; // Hide the loading screen
+        mainContent.style.display = 'block';  // Show the main content
+    }, 5000); // 10 seconds delay
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var loadingScreen = document.getElementById('other-screen');
+    var mainContent = document.getElementById('main-content');
+    
+    setTimeout(function() {
+        loadingScreen.style.display = 'none'; // Hide the loading screen
+        mainContent.style.display = 'block';  // Show the main content
+    }, 0); // 10 seconds delay
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var notesToggle = document.getElementById('notes-toggle');
+    var notesList = document.getElementById('notes-list');
+
+    notesToggle.addEventListener('click', function() {
+        if (notesList.style.display === 'none') {
+            notesList.style.display = 'block'; // Show the notes list
+        } else {
+            notesList.style.display = 'none'; // Hide the notes list
+        }
+    });
+});
