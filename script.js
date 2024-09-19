ef5654d9f167b30eefa3d1848f30d810807a419f
ManINeedToSleep <bguna0050@launchpadphilly.org>
2024-09-19 11:42:00 -0400

    Added comment for JS

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var mainContent = document.getElementById('main-content');
     var otherScreen = document.getElementById('other-screen');
 
-    
+    /* 
+    I have implemented two separate loading delays for the index.html and other pages 
+    because they each require a loading screen. While there's likely a more efficient solution, 
+    this approach ensures that each page displays the loading screen for the appropriate duration: 
+    5 seconds for the index and 0 seconds for the others.
+    */
+
+
     setTimeout(function() {
         loadingScreen.style.display = 'none'; // Hide the loading screen
         mainContent.style.display = 'block';  // Show the main content
@@ -12,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         otherScreen.style.display = 'none'; // Hide the loading screen
         mainContent.style.display = 'block';  // Show the main content
-    }, 0); // 10 seconds delay
+    }, 0); // 0 seconds delay
 });
 
 document.addEventListener("DOMContentLoaded", function() {
